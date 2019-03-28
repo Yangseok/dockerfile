1a2fb7cb33913bc05ab864646b474600c855a982
yangseok Lee <ysl0715@gmail.com>
2019-03-28 15:25:25 +0900

    docker version update

--- cli/initalizeDockerHost.sh
+++ cli/initalizeDockerHost.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 #chmod +x initalizeDockerHost.sh
 #Xenial 16.04 and newer
-sudo apt-get remove docker docker-engine docker.io
+sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
-sudo apt-get dist-upgrade
 sudo apt-get install apt-transport-https
 sudo apt-get install ca-certificates
 sudo apt-get install curl
+sudo apt-get install gnupg-agent
 sudo apt-get install software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-curl -fsSL https://get.docker.com -o get-docker.sh
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo sh get-docker.sh
-sudo usermod -aG docker $USER
-sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo apt-get install zsh
